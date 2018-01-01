4d76761a893b45b0aef7918ed29f6222ff56b60c
Steven Senkowski <scsenkowski@gmail.com>
2018-01-01 16:12:07 +0000

    automatically backed up by learn

--- db/migrate/001_create_shows.rb
+++ db/migrate/001_create_shows.rb
@@ -1,9 +1,9 @@
 class CreateShows < ActiveRecord::Migration[5.0]
   def change
     create_table :shows do |t|
-      t.string
-      t.string
-      t.string
+      t.string :name
+      t.string :network
+      t.string :day
       t.integer
   end
 end
