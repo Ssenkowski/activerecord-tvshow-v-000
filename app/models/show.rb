a17650eb38b807ae0bb4fdb2cf3c82c5f76ff1e1
Steven Senkowski <scsenkowski@gmail.com>
2018-01-01 17:05:28 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,29 @@
 class Show < ActiveRecord::Base
+  def self.highest_rating
+    Shows.maximum("rating").name
+  end
+
+  def self.most_popular_show
+    Shows.highest_rating.name
+  end
+
+  def self.lowest_rating
+    Shows.minimum("rating").name
+  end
+
+  def self.least_popular_show
+    Shows.lowest_rating.name
+  end
+
+  def self.ratings_sum
+    Shows.sum("rating")
+  end
+
+  def self.popular_shows
+    Shows.where("rating > 5").name
+  end
+
+  def self.shows_by_alphabetical_order
+    Shows.order(name: :asc)
+  end
 end
